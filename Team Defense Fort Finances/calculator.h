5b503067641128fa932666e89bd086b860b9b8fa
Jeffrey Brice <jeffbricemusic@gmail.com>
2013-11-27 10:54:52 -0800

    Added calculator class

---
+++ Team Defense Fort Finances/calculator.h
@@ -0,0 +1,13 @@
+//
+//  calculator.h
+//  Team Defense Fort Finances
+//
+//  Created by Jeffrey Brice on 11/26/13.
+//  Copyright (c) 2013 Jeffrey Brice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface calculator : NSObject
+
+@end
---
+++ Team Defense Fort Finances/calculator.m
@@ -0,0 +1,13 @@
+//
+//  calculator.m
+//  Team Defense Fort Finances
+//
+//  Created by Jeffrey Brice on 11/26/13.
+//  Copyright (c) 2013 Jeffrey Brice. All rights reserved.
+//
+
+#import "calculator.h"
+
+@implementation calculator
+
+@end
